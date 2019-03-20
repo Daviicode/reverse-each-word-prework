f4b08a89c9f835339f33333453573a22153ca61e
David zhang <d.ydzdavid@yahoo.com>
2019-03-20 08:45:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,6 +5,8 @@ def reverse_each_word (string)
 end
 
 
-def reverse_each_word (string)
-  
+def reverse_each_word (array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
 end
