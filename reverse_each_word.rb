328b78e3d7c3d4c76e7f18e0fb7e27a73ea3eb62
David zhang <d.ydzdavid@yahoo.com>
2019-03-20 08:21:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word (string)
   array = []
   str = string.length - 1 
-  string.each downto(0) do |index|
+  string.each.downto(0) do |index|
     array << str[index]
   end
  array
